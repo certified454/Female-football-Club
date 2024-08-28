a5f3610f8be0a47d2ecc99b1c191b11347237fb3
certified454 <fj689669@gmail.com>
2024-08-28 02:49:10 +0100

    commit

--- scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const toggle = document.getElementById('navbar-toggle');
-    const menu = document.getElementById('navbar-menu');
-
-    toggle.addEventListener('click', function() {
-        menu.classList.toggle('show');
-    });
-});
